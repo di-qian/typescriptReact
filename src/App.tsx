1e74cba3b1a660ef67bd6fdfec16010818c93001
Di Qian <diqian@gmail.com>
2022-01-15 01:50:00 -0500

    Lesson on Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
+import { Container } from './components/Container';
 
 function App() {
+  const personName = {
+    first: "Bruce",
+    last: "Wayne",
+  }
+  const nameList=[
+    {
+      first: 'Bruce',
+      last: 'Wayne'
+    },
+    {
+      first: 'Clark',
+      last: 'Kent'
+    },
+    {
+      first: 'Princess',
+      last: 'Diana'
+    },
+  ]
   return (
     <div className="App">
       <Greet name='Di Qian' messageCount={20} isLoggedIn={false}/>
+      <Greet name='Di Qian' isLoggedIn={true}/>
+      <Person name={personName}/>
+      <PersonList names={nameList} />
+      <Status status='error'/>
+      <Heading>123</Heading>
+      <Oscar><Heading>Oscar goes to Leonardo Dicaprio</Heading></Oscar>
+      <Button handleClick={(event, id)=>{
+        console.log("Button Clicked", event, id)
+      }}/>
+      <Input value='' handleChange={(event)=>console.log(event)} />
+      <Container styles={{border: '1px solid black', padding: '1rem'}} />
     </div>
   );
 }
